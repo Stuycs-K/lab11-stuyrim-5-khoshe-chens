e3ad7d2b7f011a600a0786be77c22f39eda867c4
schen0523 <schen65@stuy.edu>
2025-01-13 11:42:54 -0500

    added Priest constructor and abstract method headers

---
+++ Priest.java
@@ -0,0 +1,30 @@
+public class Priest extends Adventurer{
+  //priest special is called mana for now, is subject to change
+  int mana, manaMax;
+
+  public Priest(String name, int hp) {
+
+  }
+  //add overloaded contructors if necessary
+
+  public String getSpecialName() {
+    return "";
+  }
+
+  public int getSpecial() {
+    return 0;
+  }
+
+  public int getSpecialMax() {
+    return 0;
+  }
+
+  public void setSpecial(int n) {
+
+  }
+
+  
+
+
+
+}
